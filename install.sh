fc2a5e0f9ca2fd3ca24eb65b6b51e4f9fccc42eb
Josuke <84533536+TheGrandTrrr@users.noreply.github.com>
2026-02-09 18:07:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,53 @@
 #!/bin/bash
+
 set -e
 
-echo " Installing Battery health script ... "
 if [[ $EUID -ne 0 ]]; then
-	echo "Run as root. (sudo ./install.sh)"
-	exit 1
+  echo "Run as root. (sudo ./install.sh)"
+  exit 1
 fi
 
-cp "bthlt" "/usr/local/bin"
+echo "==== Battery Health Monitor installer ===="
+echo ""
+echo "Choose a verison you like"
+echo ""
+echo "    1) Only text"
+echo "    2) Text + picture"
+echo "    3) Stop installation"
+echo ""
+echo -n "Write a number with a version you pick:     "
+read choise
 
-chmod +x "/usr/local/bin/bthlt"
+case $choise in
+1)
+  echo "Downloading text version of a programm."
 
-echo "Script was installed"
-echo "Run command  'bthlt' to use a script"
+  cp "versions/bthlt" "/usr/local/bin"
 
+  chmod +x "/usr/local/bin/bthlt"
+
+  echo "Script was installed"
+  echo ""
+  echo "Run command  'bthlt' to use a script"
+  ;;
+2)
+  echo "Dowloading full version of a programm."
+
+  cp "versions/bthlt_b" "/usr/local/bin/bthlt"
+
+  chmod +x "/usr/local/bin/bthlt"
+
+  echo "Script was installed"
+  echo ""
+  echo "Run command  'bthlt' to use a script"
+  ;;
+3)
+  echo "Stopping installation..."
+  exit 1
+  ;;
+*)
+  echo "Wrong choise. Exiting an installation script..."
+  exit 1
+  ;;
+
+esac
